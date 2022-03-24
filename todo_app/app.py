510da86d1c52f0972981a3d032d320eb63e7fbaf
Paul Curren <paul.curren@necsws.com>
2022-03-24 11:39:47 +0000

    update

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import add_item, get_items
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello World!'
+    if (request.method == 'GET'):
+        return render_template('index.html', items = get_items())
+
+    if (request.method == 'POST'):
+        title = request.form.get('title')
+        add_item(title)
+        return render_template('index.html', items = get_items())
+
+
