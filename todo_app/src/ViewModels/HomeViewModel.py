e187a244f70573c43c5661382703e61cec6adbb3
Paul Curren <paul.curren@necsws.com>
2022-05-12 16:39:21 +0100

    refactoring and more tests

--- todo_app/src/ViewModels/HomeViewModel.py
+++ todo_app/src/ViewModels/HomeViewModel.py
@@ -12,13 +12,25 @@ class HomeViewModel:
         
     @property
     def doing_items(self) -> list[Item]:
-        result = []
-        for item in self._items:
-            if (item.status == "Doing"):
-                result.append(item)
-        return result
+        return self.__items_by_status("Doing")
+
+    @property
+    def done_items(self) -> list[Item]:
+        return self.__items_by_status("Done")
+
+    @property
+    def todo_items(self) -> list[Item]:
+        return self.__items_by_status("To Do")
 
 
     @property
     def statuses(self) -> list[str]:
         return self._statuses
+
+    def __items_by_status(self, status) -> list[Item]:
+        result = []
+        for item in self._items:
+            if (item.status == status):
+                result.append(item)
+        return result
+    
--- todo_app/tests/test_home_view_model.py
+++ todo_app/tests/test_home_view_model.py
@@ -19,3 +19,34 @@ def test_doing_items():
     assert len(doing_items) == 2
     assert doing_items[0].title == "Item1"
     assert doing_items[1].title == "Item3"
+
+def test_done_items():
+    # arrange
+    item1 = Item(1, "Doing", "Item1")
+    item2 = Item(2, "Done", "Item2")
+    item3 = Item(3, "Doing", "Item3")
+    
+    view_model = HomeViewModel(items=[item1, item2, item3], statuses=[])
+
+    # act
+    done_items = view_model.done_items
+
+    # assert
+    assert len(done_items) == 1
+    assert done_items[0].title == "Item2"
+
+def test_todo_items():
+    # arrange
+    item1 = Item(1, "Doing", "Item1")
+    item2 = Item(2, "Done", "Item2")
+    item3 = Item(3, "Doing", "Item3")
+    item3 = Item(4, "To Do", "Item4")
+    
+    view_model = HomeViewModel(items=[item1, item2, item3], statuses=[])
+
+    # act
+    todo_items = view_model.todo_items
+
+    # assert
+    assert len(todo_items) == 1
+    assert todo_items[0].title == "Item4"
